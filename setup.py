7600c4ee1a372fd1d2b6895719bbf484caf5794f
Kartik Sharma <kartik.sharma@myntra.com>
2020-02-16 16:33:25 +0530

    Remove TODOs

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ import setuptools
 setuptools.setup(name='crop-transparent-image',
                  version='0.1',
                  description='Trim unnecessary transparent padding from your images',
-                 # TODO: Uncomment after uploading project to github
-                 # url='http://github.com/crazyhitty/crop-transparent-image',
+                 url='https://github.com/crazyhitty/crop-transparent-image',
                  author='Kartik Sharma',
                  author_email='kartik.sharma.og@gmail.com',
-                 # TODO: Uncomment after adding license to project
-                 # license='MIT',
+                 license='MIT',
                  packages=setuptools.find_packages(),
                  zip_safe=False,
                  install_requires=['Pillow'])
